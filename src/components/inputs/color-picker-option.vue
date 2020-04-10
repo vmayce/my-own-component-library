e405c356a0e43748a151cad25a0b7fa3b9d619ff
May <4741503+vmayce@users.noreply.github.com>
2020-04-09 22:21:37 -0400

    Added in the initial project from the Vue-Cli defaults.

---
+++ src/components/inputs/color-picker-option.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="color-picker-option">
+    <input type="color" />
+    <input type="text" />
+    <select>
+      <option value="">M(eters)</option>
+      <option value="">F(ee)t</option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'ColorPickerOption',
+    components: {
+    },
+  };
+</script>
---
+++ src/views/Components.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="components">
+    <color-picker-option></color-picker-option>
+  </div>
+</template>
+
+<script>// @ is an alias to /src
+import ColorPickerOption from '@/components/inputs/color-picker-option.vue';
+
+export default {
+  name: 'Components',
+  components: {
+    'color-picker-option' : ColorPickerOption,
+  },
+};</script>
